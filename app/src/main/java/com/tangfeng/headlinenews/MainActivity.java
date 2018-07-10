ef6a4212144cc621eab816afb834c7e6a88dc242
汪唐风 <843369849@qq.com>
2018-07-10 15:51:06 +0800

    1

---
+++ app/src/main/java/com/tangfeng/headlinenews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tangfeng.headlinenews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
