c153878ca180c978ab791bcc0d414afac2ef84b9
汪唐风 <843369849@qq.com>
2018-07-20 23:22:32 +0800

    drawer_layout

--- app/src/main/java/com/tangfeng/headlinenews/MainActivity.java
+++ app/src/main/java/com/tangfeng/headlinenews/MainActivity.java
@@ -1,18 +1,26 @@
 package com.tangfeng.headlinenews;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.tangfeng.headlinenews.module.base.BaseActivity;
+import com.tangfeng.headlinenews.util.SettingUtil;
 import com.tangfeng.headlinenews.widget.helper.BottomNavigationViewHelper;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar toolbar;
     private BottomNavigationView bottom_navigation;
+    private DrawerLayout drawer_layout;
+    private NavigationView nav_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,5 +55,43 @@ public class MainActivity extends BaseActivity {
                 return true;
             }
         });
+
+        drawer_layout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer_layout,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer_layout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        nav_view = findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            //切换主题
+            case R.id.nav_switch_night_mode:
+                //系统自带模式切换日夜间模式
+                int mode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+                if (mode == Configuration.UI_MODE_NIGHT_YES) {
+                    SettingUtil.getInstance().setIsNightMode(false);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                } else {
+                    SettingUtil.getInstance().setIsNightMode(true);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                //渐变
+                getWindow().setWindowAnimations(R.style.WindowAnimationFadeInOut);
+                recreate();
+                return false;
+            case R.id.nav_setting:
+
+                return false;
+            case R.id.nav_share:
+
+                return false;
+
+        }
+        return false;
     }
 }
